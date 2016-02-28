c1e94851a644bd5df55ff24a2dba910ba3f4e8d4
Jonathan Schneider (shbi) <lespaulgothstud@gmail.com>
2016-02-28 12:41:11 -0500

    Add JUnit reflection  example tests

---
+++ app/src/main/java/com/mobiquity/androidunittests/NeedsTesting.java
@@ -0,0 +1,101 @@
+package com.mobiquity.androidunittests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Stupid example of a class that might need testing.
+ *
+ * README:
+ *     This class is stupid. It's meant as a simple example.
+ *     Hopefully I can come up with a better example in the future and provide something more
+ *     concrete.
+ *     The example itself is useful as it can provide a null safe Integer list. As long as values
+ *     are added and removed from the interface you know it will not have a null.
+ *
+ *     handle(int): This method does evil and confusing things. But it makes a good example here.
+ *                  It takes a value and adds it to the list if it's already not there or removes it
+ *                  if it is already there. This is why it's confusing. The caller has no way of
+ *                  knowing the behavior of this method unless it knows the state of the list.
+ *
+ *     get() & get(int, int): Gets a filtered version of this list where the values are in
+ *                            [min, max]. get() uses the default range of [0, 100].
+ *
+ *     clearList(): does what it says, clears the list, makes sense.
+ *
+ *     isInRange(int, int, int): Method is unnecessary honestly, but I needed a private method
+ *                               example.
+ */
+public class NeedsTesting {
+    /** Max value allowed in list. */
+    private static final int MAX = 100;
+    /** Min value allowed in list. */
+    private static final int MIN = 0;
+    /** List of values being stored. */
+    private final List<Integer> integerList = new ArrayList<>();
+
+    /**
+     * Method for handling values being passed the class that needs testing.
+     * This method will add valid values to the list, or remove them if they already exist.
+     * NOTE: This is an example of a BAD method as the result of it is unknown at any time.
+     * It's better to have a add method and remove method explicitly. But it makes for a good
+     * example of a test.
+     *
+     * @param value value to handle.
+     */
+    public void handle(final int value) {
+        if (integerList.contains(value)) {
+            integerList.remove(integerList.indexOf(value));
+        } else {
+            integerList.add(value);
+        }
+    }
+
+    /**
+     * Gets a list of values within the default range.
+     *
+     * @return a list of values within the default range.
+     */
+    public List<Integer> get() {
+        return get(MIN, MAX);
+    }
+
+    /**
+     * Gets a list of values within the specified range.
+     *
+     * @param min min value of range.
+     * @param max max value of range.
+     * @return a list of values within the specified range.
+     */
+    public List<Integer> get(final int min, final int max) {
+        if (max < min) {
+            throw new IllegalArgumentException(
+                String.format("Max(%d) should not be less than Min(%d)", max, min));
+        }
+        final List<Integer> result = new ArrayList<>();
+        for (final Integer value : integerList) {
+            if (!isInRange(value, min, max)) continue;
+            result.add(value);
+        }
+        return result;
+    }
+
+    /**
+     * private method to clear the list.
+     */
+    protected void clearList() {
+        integerList.clear();
+    }
+
+    /**
+     * Private method for testing if a value is within the range.
+     *
+     * @param value value to test.
+     * @param min min value of range.
+     * @param max max value of range.
+     * @return true if the value is within the range. false otherwise.
+     */
+    private static boolean isInRange(final int value, final int min, final int max) {
+        return min <= value && value <= max;
+    }
+}
---
+++ app/src/test/java/com/mobiquity/androidunittests/mock/MockExecutorService.java
@@ -0,0 +1,138 @@
+package com.mobiquity.androidunittests.mock;
+
+import android.support.annotation.NonNull;
+
+import java.util.AbstractQueue;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * Mock implementation of an ExecutorService.
+ * With Unit tests you don't want to test threaded code.
+ * It's usually easier and better to test deterministic code.
+ * This mock will allow you to test code that delegates to another
+ * thread by intercepting that delegation. You would only need to implement
+ * the interfaces you need.
+ */
+public class MockExecutorService implements ExecutorService {
+    /** Flag for if the executor is shut down. */
+    private boolean isShutdown;
+    /** A Queue of Runnables.  */
+    private Queue<Runnable> runnableQueue = new ArrayDeque<>();
+
+    /**
+     * Maybe we want to run all the tasks at a time we have control over.
+     */
+    public void runAllTasks() {
+        for (final Runnable runnable : runnableQueue) {
+            runnable.run();
+        }
+        runnableQueue.clear();
+    }
+
+    /**
+     * Get a count of the pending tasks.
+     *
+     * @return count of the pending tasks.
+     */
+    public int getPendingCount() {
+        return runnableQueue.size();
+    }
+
+    @Override
+    public void shutdown() {
+        isShutdown = true;
+    }
+
+    @NonNull
+    @Override
+    public List<Runnable> shutdownNow() {
+        shutdown();
+        return new ArrayList<>();
+    }
+
+    @Override
+    public boolean isShutdown() {
+        return isShutdown;
+    }
+
+    @Override
+    public boolean isTerminated() {
+        return isShutdown;
+    }
+
+    @Override
+    public boolean awaitTermination(
+        final long timeout, final TimeUnit unit) throws InterruptedException {
+
+        return true;
+    }
+
+    @NonNull
+    @Override
+    public <T> Future<T> submit(final Callable<T> task) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public <T> Future<T> submit(final Runnable task, final T result) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public Future<?> submit(final Runnable task) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public <T> List<Future<T>> invokeAll(
+        final Collection<? extends Callable<T>> tasks) throws InterruptedException {
+
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public <T> List<Future<T>> invokeAll(
+        final Collection<? extends Callable<T>> tasks,
+        final long timeout,
+        final TimeUnit unit) throws InterruptedException {
+
+        return null;
+    }
+
+    @NonNull
+    @Override
+    public <T> T invokeAny(
+        final Collection<? extends Callable<T>> tasks)
+        throws InterruptedException, ExecutionException {
+
+        return null;
+    }
+
+    @Override
+    public <T> T invokeAny(
+        final Collection<? extends Callable<T>> tasks,
+        final long timeout,
+        final TimeUnit unit) throws InterruptedException, ExecutionException, TimeoutException {
+
+        return null;
+    }
+
+    @Override
+    public void execute(final Runnable task) {
+        runnableQueue.add(task);
+    }
+}
