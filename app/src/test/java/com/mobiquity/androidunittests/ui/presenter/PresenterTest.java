c48ed48315e33dba93322e1f3fb454a0625a91e7
Justin Washington <jwashington@mobiquityinc.com>
2016-04-01 16:36:17 -0400

    unit test for base presenter class

---
+++ app/src/test/java/com/mobiquity/androidunittests/ui/presenter/PresenterTest.java
@@ -0,0 +1,57 @@
+package com.mobiquity.androidunittests.ui.presenter;
+
+import com.mobiquity.androidunittests.ui.mvpview.MvpView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static com.google.common.truth.Truth.*;
+
+public class PresenterTest {
+
+
+    private Presenter<MvpView> presenter;
+    @Mock MvpView view;
+
+    @Before
+    public void setUp() {
+        presenter = new Presenter<MvpView>() {};
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testBindView_ShouldBindViewToPresenter() {
+        presenter.bind(view);
+        assertThat(presenter.view()).isEqualTo(view);
+    }
+
+    @Test
+    public void testView_IsNullByDefault() {
+        assertThat(presenter.view()).isNull();
+    }
+
+    @Test
+    public void testIsViewAttached_IsTrueAfterBind() {
+        assertThat(presenter.isViewAttached()).isFalse();
+        presenter.bind(view);
+        assertThat(presenter.isViewAttached()).isTrue();
+    }
+
+    @Test
+    public void testUnbindView_ShouldBeNullAfterUnbind() {
+        presenter.bind(view);
+        presenter.unbind();
+        assertThat(presenter.view()).isNull();
+    }
+
+    @Test
+    public void testIsViewAttached_IsFalseAfterUnind() {
+        assertThat(presenter.isViewAttached()).isFalse();
+        presenter.bind(view);
+        presenter.unbind();
+        assertThat(presenter.isViewAttached()).isFalse();
+    }
+
+}
